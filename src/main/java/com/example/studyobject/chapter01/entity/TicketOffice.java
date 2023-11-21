88f7393fd2e6aa20ea8ba0c23319af9cf06381c8
LeeSeungJu <exceedlimit94@gmail.com>
2023-11-21 23:04:56 +0900

    [chapter01] TicketOffice에게 자율성 부여

--- src/main/java/com/example/studyobject/chapter01/entity/TicketOffice.java
+++ src/main/java/com/example/studyobject/chapter01/entity/TicketOffice.java
@@ -9,18 +9,22 @@ import java.util.List;
 public class TicketOffice {
 
     private Long amount;
-    private List<Ticket> tickets = new ArrayList<>();
+    private List<Ticket> tickets;
 
-    public Ticket getTicket(){
+    private Ticket getTicket(){
         return tickets.remove(0);
     }
 
-    public void minusAmount(Long amount){
+    private void minusAmount(Long amount){
         this.amount -= amount;
     }
 
-    public void plusAmount(Long amount){
+    private void plusAmount(Long amount){
         this.amount += amount;
+    }
+
+    public void sellTicketTo(Audience audience){
+        plusAmount(audience.buy(getTicket()));
     }
 
 }
