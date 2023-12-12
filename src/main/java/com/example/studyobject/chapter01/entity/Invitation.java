33846e1fd56da392dbca727f39e473c500650fd7
lsj <exceedlimit94@gmail.com>
2023-12-12 16:51:10 +0900

    [chpater01] 오류 해결

--- src/main/java/com/example/studyobject/chapter01/entity/Invitation.java
+++ src/main/java/com/example/studyobject/chapter01/entity/Invitation.java
@@ -1,11 +1,13 @@
 package com.example.studyobject.chapter01.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 
 @AllArgsConstructor
+@Getter
 public class Invitation {
     private LocalDateTime when;
 }
