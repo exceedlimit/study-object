4a8a6c0db4fabfd7c96d0628fd58988f2ba1e8a8
lsj <exceedlimit94@gmail.com>
2023-12-13 16:50:02 +0900

    [chapter04] 캡슐화 개선

--- src/main/java/com/example/studyobject/chapter04/entity/Movie.java
+++ src/main/java/com/example/studyobject/chapter04/entity/Movie.java
@@ -1,5 +1,6 @@
 package com.example.studyobject.chapter04.entity;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import lombok.Getter;
@@ -15,4 +16,42 @@ public class Movie {
     private MovieType movieType;
     private Money discountAmount;
     private double discountPercent;
+
+    public Money calculateAmoutDiscountedFee(){
+        if(movieType != MovieType.AMOUNT_DISCOUNT){
+            throw new IllegalArgumentException();
+        }
+        return fee.minus(discountAmount);
+    }
+
+    public Money calculatePercentDiscountedFee(){
+        if(movieType != MovieType.PERCENT_DISCOUNT){
+            throw new IllegalArgumentException();
+        }
+
+        return fee.minus(fee.times(discountPercent));
+    }
+
+    public Money calculateNoneDiscountedFee(){
+        if(movieType != MovieType.NONE_DISCOUNT){
+            throw new IllegalArgumentException();
+        }
+
+        return fee;
+    }
+
+    public boolean isDiscountable(LocalDateTime whenScreened, int sequence){
+        for(DiscountCondition condition : discountConditions){
+            if(condition.getType() == DiscountConditionType.PERIOD){
+                if(condition.isDiscountable(whenScreened.getDayOfWeek(), whenScreened.toLocalTime())){
+                    return true;
+                }
+            } else{
+                if(condition.isDiscountable(sequence)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
--- src/main/java/com/example/studyobject/chapter04/entity/Screening.java
+++ src/main/java/com/example/studyobject/chapter04/entity/Screening.java
@@ -2,12 +2,32 @@ package com.example.studyobject.chapter04.entity;
 
 import java.time.LocalDateTime;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter@Setter
+@AllArgsConstructor
 public class Screening {
     private Movie movie;
     private int sequence;
     private LocalDateTime whenScreened;
+
+    public Money calculateFee(int audienceCount){
+        switch (movie.getMovieType()) {
+            case AMOUNT_DISCOUNT:
+                if(movie.isDiscountable(whenScreened, sequence)){
+                    return movie.calculateAmoutDiscountedFee().times(audienceCount);
+                }
+                break;
+            case PERCENT_DISCOUNT:
+                if(movie.isDiscountable(whenScreened, sequence)){
+                    return movie.calculatePercentDiscountedFee().times(audienceCount);
+                }
+            case NONE_DISCOUNT:
+                return movie.calculateNoneDiscountedFee().times(audienceCount);
+        }
+
+        return movie.calculateNoneDiscountedFee().times(audienceCount);
+    }
 }
